97dfdac54e274504dab7906f1ceeed683a53a002
root <root@ip-172-31-2-6.us-east-2.compute.internal>
2021-05-17 09:36:53 +0000

    Day1_History.txt

--- 00-Setup/install-mailhog.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Install docker from Docker-ce repository
-echo "[TASK 1] Setup MailHog Mail Server"
-docker run --restart unless-stopped --name mailserver -p 1025:1025 -p 8025:8025 -d mailhog/mailhog
